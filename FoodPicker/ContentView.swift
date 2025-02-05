592259d3411860b1aec1fb77896fba82c9f531c6
yufei <yufei5209@163.com>
2025-02-05 21:09:43 +0800

    Initial Commit

---
+++ FoodPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker
+//
+//  Created by yufei on 2025/2/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by yufei on 2025/2/5.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
